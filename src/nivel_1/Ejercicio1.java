92a02dd808f1b6e89d1bb1602d91963eeededcaa
AgustinBen <agustinbengolea97@gmail.com>
2026-01-21 11:21:58 -0300

    initial commit

---
+++ src/nivel_1/Ejercicio1.java
@@ -0,0 +1,21 @@
+package nivel_1;
+
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		int primerNumero = 20;
+		int segundoNumero = 3;
+		int suma = primerNumero + segundoNumero;
+		int resta = primerNumero - segundoNumero;
+		int multiplicacion = primerNumero * segundoNumero;
+		double division = (double) primerNumero / segundoNumero;
+		int resto = primerNumero % segundoNumero;
+		
+		System.out.println("Suma: " + suma);
+		System.out.println("Resta: " + resta);
+		System.out.println("Multiplicacion: " + multiplicacion);
+		System.out.println("Division: " + division);
+		System.out.println("Resto: " + resto);
+	}
+}
