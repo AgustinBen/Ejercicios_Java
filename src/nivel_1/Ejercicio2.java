6ced7dc44dd22018d2a28c1a3421ad3110a25cff
AgustinBen <agustinbengolea97@gmail.com>
2026-01-22 09:58:11 -0300

    Ejercicio 2 completado.

---
+++ src/nivel_1/Ejercicio2.java
@@ -0,0 +1,18 @@
+package nivel_1;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		int numero = -10; //Cambiar el valor para probar distintos casos.
+		
+		if (numero > 0) {
+			System.out.println("El número es positivo");
+		}
+		else if(numero < 0) {
+			System.out.println("El número es negativo");
+		}
+		else {
+			System.out.println("El número es cero");
+		}
+	}	
+}
