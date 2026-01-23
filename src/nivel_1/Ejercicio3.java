d95cd75c21c40a5bf43fd639eda64d1a6f2c382a
AgustinBen <agustinbengolea97@gmail.com>
2026-01-23 17:03:38 -0300

    Ejercicio 3 terminado

---
+++ src/nivel_1/Ejercicio3.java
@@ -0,0 +1,26 @@
+package nivel_1;
+
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		int numero = 0;
+		
+		if(numero % 2 == 0) {
+			System.out.println("El número es par");	
+		}
+		else {
+			System.out.println("El número es impar");
+		}
+	}
+}
+
+//switch (numero % 2){
+//case 0:
+//	System.out.println("El número es par");
+//	break;
+//default: 
+//	System.out.println("El número es impar");
+//	break;
+//}
