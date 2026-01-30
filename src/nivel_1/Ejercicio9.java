fc98434904f15d514f9363eb971dfc9b254fb763
AgustinBen <agustinbengolea97@gmail.com>
2026-01-29 22:38:42 -0300

    Ejercicio 9

---
+++ src/nivel_1/Ejercicio9.java
@@ -0,0 +1,22 @@
+package nivel_1;
+
+public class Ejercicio9 {
+
+	public static void main(String[] args) {
+		int limite = 10;
+		mostrarNumerosDel1AlN(limite);
+
+	}
+	
+	public static void mostrarNumerosDel1AlN(int n) {
+		
+		if (n > 0) {
+			for (int i = 1; i <= n ; i++ ) {
+				System.out.println(i);
+			}
+		} else {
+			System.out.println("El número debe ser mayor a 0");
+		}
+	}
+
+}
